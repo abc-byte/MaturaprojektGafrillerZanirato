116ff45b96cf05fb88d833e4eac0f81b1c271ba1
unknown <zani>
2024-01-09 09:32:54 +0100

    bootstrap und website hinzugefügt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JobListComponent } from './job-list/job-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/job-list/job-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mp-job-list',
+  templateUrl: './job-list.component.html',
+  styleUrls: ['./job-list.component.scss']
+})
+export class JobListComponent {
+
+}
